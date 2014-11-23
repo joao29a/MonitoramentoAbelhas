46a400fcb9d7973a3d9ecb1a13fd59102c6f53ff
Joao Almeida Jr <joao29a@gmail.com>
2014-11-23 02:18:45 -0200

    exemplos com o rasp

--- examples/generic/clientgeneric.cpp
+++ examples/generic/clientgeneric.cpp
@@ -6,20 +6,31 @@
 #include <math.h>
 #include <vector>
 #include <string>
+#include <time.h>
+#include <bcm2835.h>
 
 #define TYPE Generic
 
-uint16_t size = 0;
+uint16_t port = 0;
+time_t bf_time = 0;
 
 TYPE GetGenericData() {
-  stringstream ss;
-  ss << "generic-data-" << size;
-  Generic data(ss.str(), size);
-  size++;
-  return data;
+  while (true) {
+    sleep(1);
+    time_t diff = time(NULL) - bf_time;
+    if (bcm2835_gpio_lev(port) && diff > 30) {
+      time(&bf_time);
+      Generic data("Movement detected!", 1);
+      return data;
+    }
+  }
 }
 
 int main(int argc, char **argv) {
+  bcm2835_init();
+
+  bcm2835_gpio_fsel(port, BCM2835_GPIO_FSEL_INPT);
+
   ClientMonitor<TYPE> monitor(&argc, argv);
 
   monitor.AddDataGenerator("generic data", &GetGenericData);
